b98fe7e09ae8575552db95478704f929f45ca7bd
raufzer <a_zerkhef@estin.dz>
2024-08-25 16:08:43 +0100

    Commit

---
+++ lib/models/article_model.dart
@@ -0,0 +1,13 @@
+class ArticelModel {
+  final String? image;
+  final String title;
+  final String articleWriter;
+  final String articleDate;
+
+  ArticelModel({
+    required this.image,
+    required this.title,
+    required this.articleWriter,
+    required this.articleDate,
+  });
+}
--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -1,15 +1,26 @@
+import 'package:al24news_app/models/article_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class NewsService {
-String apiKey = dotenv.env['API_KEY']!;
-final Dio dio;
+  String apiKey = dotenv.env['API_KEY']!;
+  final Dio dio;
 
-NewsService(this.dio);
+  NewsService(this.dio);
 
-void getLastetNews() async {
-  Response response = await dio.get('https://newsdata.io/api/1/latest?country=dz,ma,tn,ly,ne&language=en&apikey=$apiKey');
-
-
-}
+  void getLastetNews() async {
+    Response res = await dio.get(
+        'https://newsdata.io/api/1/latest?country=dz,ma,tn,ly,ne&language=en&apikey=$apiKey');
+    Map<String, dynamic> data = res.data;
+    List<dynamic> articles = data['results'];
+    List<ArticelModel> articlesList = [];
+    for (var article in articles) {
+      ArticelModel articelModel = ArticelModel(
+          image: article['image_url'],
+          title: article['title'],
+          articleWriter: article['creator'],
+          articleDate: article['pubDate']);
+      articlesList.add(articelModel);
+    }
+  }
 }
