dc52f3e6793550595c25c78f6a16a7da9e262b2a
raufzer <a_zerkhef@estin.dz>
2024-08-29 17:10:56 +0100

    code refactor

--- lib/widgets/latest_news_feed_widget_builder.dart
+++ lib/widgets/latest_news_feed_widget_builder.dart
@@ -1,41 +1,21 @@
-import 'package:al24news_app/models/article_model.dart';
 import 'package:al24news_app/services/news_service.dart';
 import 'package:al24news_app/widgets/latest_news_feed_widge.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-class LastetNewsFeedBuilder extends StatefulWidget {
+class LastetNewsFeedBuilder extends StatelessWidget {
   const LastetNewsFeedBuilder({
     super.key,
   });
 
   @override
-  State<LastetNewsFeedBuilder> createState() => _LastetNewsFeedBuilderState();
-}
-
-class _LastetNewsFeedBuilderState extends State<LastetNewsFeedBuilder> {
-  List<ArticleModel> articles = [];
-  bool isLoading = true;
-  @override
-  void initState() {
-    super.initState();
-    getLastetNews();
-  }
-
-  Future<void> getLastetNews() async {
-    articles = await NewsService(dio: Dio()).getLatestNews();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return isLoading
-        ? const CircularProgressIndicator()
-        : articles.isNotEmpty ? LastetNewsFeed(
-            articles: articles,
-          ) : const Center(
-            child: Text('No data found'),
+    return FutureBuilder(
+        future: NewsService(dio: Dio()).getLatestNews(),
+        builder: (context, snapshot) {
+          return LastetNewsFeed(
+            articles: snapshot.data ?? [],
           );
+        });
   }
 }
