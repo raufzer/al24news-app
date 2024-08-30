2e50ab47886b9f7b3cbd124d1e6eecda43a8cf1b
raufzer <a_zerkhef@estin.dz>
2024-08-30 23:02:12 +0100

    Code Refactor

--- lib/widgets/latest_news_feed_widget_builder.dart
+++ lib/widgets/latest_news_feed_widget_builder.dart
@@ -1,21 +1,53 @@
+import 'package:al24news_app/models/article_model.dart';
 import 'package:al24news_app/services/news_service.dart';
 import 'package:al24news_app/widgets/latest_news_feed_widge.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-class LastetNewsFeedBuilder extends StatelessWidget {
+// class LastetNewsFeedBuilder extends StatelessWidget {
+//   const LastetNewsFeedBuilder({
+//     super.key,
+//   });
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder(
+//         future: NewsService(dio: Dio()).getLatestNews(),
+//         builder: (context, snapshot) {
+//           return LastetNewsFeed(
+//             articles: snapshot.data ?? [],
+//           );
+//         });
+//   }
+// }
+
+class LastetNewsFeedBuilder extends StatefulWidget {
   const LastetNewsFeedBuilder({
     super.key,
   });
 
   @override
+  State<LastetNewsFeedBuilder> createState() => _LastetNewsFeedBuilderState();
+}
+
+class _LastetNewsFeedBuilderState extends State<LastetNewsFeedBuilder> {
+  List<ArticleModel> articles = [];
+  bool isLoading = true;
+  @override
+  void initState() {
+    super.initState();
+    getNewsByCategory();
+  }
+
+  Future<void> getNewsByCategory() async {
+    articles = await NewsService(dio: Dio()).getLatestNews();
+    isLoading = false;
+    setState(() {});
+  }
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: NewsService(dio: Dio()).getLatestNews(),
-        builder: (context, snapshot) {
-          return LastetNewsFeed(
-            articles: snapshot.data ?? [],
-          );
-        });
+    return isLoading
+        ? const CircularProgressIndicator()
+        : LastetNewsFeed(articles: articles);
   }
 }
--- tempCodeRunnerFile.dart
+++
@@ -1,8 +0,0 @@
-// future with delay example 
-import 'dart:async';
-
-void main() async{
-  print('Fetching data...');
-  await Future.delayed(const Duration(seconds: 2));
-  print('Data fetched.');
-}
